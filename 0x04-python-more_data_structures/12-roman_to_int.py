ff18a69de85e8a8157eb27b02e6aac68b9649e9e
amnaybenhaddou <amnaybenhaddou98@gmail.com>
2023-12-08 11:50:13 -0800

    More data structures in python

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_numeral):
     if not roman_numeral or type(roman_numeral) != str:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
     result = 0
     prev_value = 0
     for i in range(len(roman_numeral)):
